41e5c991e007405d7c068e89fb544fee6bc5c5e0
huanranc <huanranjoe@163.com>
2018-03-08 22:45:18 -0800

    update req

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const koaLogger = require('koa-logger');
 var cors = require('koa-cors');
 const apicache = require("apicache");
 const serve = require('koa-static');
+const { createWebAPIRequest } = require('./util/util');
 
 
 const app = new Koa();
-const router = new Router();
 
 // // session存储配置
 // const sessionMysqlConfig= {
@@ -40,9 +40,14 @@ app.use(cors());
 
 // app.use = serve(path.join(__dirname));
 
+// const onlyStatus200 = (ctx, next) => ctx.status === 200;
+// let cache = apicache.middleware;
+
+// app.use(cache("2 minutes", onlyStatus200));
+
 app.use(async (ctx,next) => {
-  ctx.type="text/html";
-  ctx.body = 'music electron';
+  // ctx.type="text/html";
+  // ctx.body = 'music electron';
   const proxy=ctx.query.proxy;
   if (proxy) {
     ctx.req.headers.cookie = ctx.req.headers.cookie + `__proxy__${proxy}`;
@@ -50,22 +55,42 @@ app.use(async (ctx,next) => {
   await next();
 });
 
-
-router.get('/',async ctx => {
+let home = new Router()
+home.get('/',async ctx => {
   ctx.body = 'music electron';
 })
 
-// 获取专辑内容
-router.get("/album", async ctx => {
-  ctx.body=require("./router/album");
-});
+let album = new Router()
 
-router.get('/hello',async(ctx,next)=>{
-  ctx.body='hello'
+// 获取专辑内容
+album.get('/',async(ctx,next)=>{
+  const cookie = ctx.cookies.get('Cookie') ? ctx.cookies.get('Cookie') : ''
+  const data ={
+    csrf_token: ''
+  }
+  const id=ctx.query.id
+  createWebAPIRequest(
+    'music.163.com',
+    `/weapi/v1/album/${id}`,
+    'POST',
+    data,
+    cookie,
+    music_req => {
+      console.log(music_req);
+      ctx.body = 'music electron'
+    }
+  )
+  ctx.body = 'music electron'
 })
 
-app.use(router.routes())
 
-const port = process.env.PORT || 3002;
+let router = new Router();
+
+router.use('/',home.routes(),home.allowedMethods())
+router.use('/album',album.routes(),album.allowedMethods())
+
+app.use(router.routes()).use(router.allowedMethods())
+
+const port = process.env.PORT || 3004
 
 app.listen(port,()=>{console.log(`localhost:${port}`)});
