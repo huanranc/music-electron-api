a2d4c1549dbfa5244c929374e0022bc3d177c0c6
huanran <huanranjoe@163.com>
2018-03-09 08:00:56 +0800

    尝试请求album

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const convert = require('koa-convert')
 const config = require('./config/config');
 const bodyParser = require('koa-bodyparser')
 const koaLogger = require('koa-logger');
+var cors = require('koa-cors');
+const apicache = require("apicache");
+const serve = require('koa-static');
+
 
 const app = new Koa();
 const router = new Router();
@@ -31,15 +35,30 @@ app.use(convert(koaLogger()))
 // 配置ctx.body解析中间件
 app.use(bodyParser())
 
+// 跨域设置
+app.use(cors());
+
+// app.use = serve(path.join(__dirname));
+
 app.use(async (ctx,next) => {
-  // ctx.type="text/html";
-  // ctx.body = 'music electron';
+  ctx.type="text/html";
+  ctx.body = 'music electron';
+  const proxy=ctx.query.proxy;
+  if (proxy) {
+    ctx.req.headers.cookie = ctx.req.headers.cookie + `__proxy__${proxy}`;
+  }
   await next();
 });
+
 
 router.get('/',async ctx => {
   ctx.body = 'music electron';
 })
+
+// 获取专辑内容
+router.get("/album", async ctx => {
+  ctx.body=require("./router/album");
+});
 
 router.get('/hello',async(ctx,next)=>{
   ctx.body='hello'
