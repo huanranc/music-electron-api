db3906cb1a9f81c00e65028e519862683814c675
Max Sky <max940213@live.com>
2018-03-16 09:51:49 +0800

    修改 .gitignore 忽略列表，默认忽略 ./config/config.js

---
+++ config/config.default.js
@@ -0,0 +1,14 @@
+const config = {
+    //启动窗口
+    port: 3000,
+    // 数据库配置
+    database: {
+        DATABASE: '',
+        USERNAME: '',
+        PASSWORD: '',
+        HOST: '127.0.0.1',
+        PORT: 3306
+    }
+};
+
+module.exports = config;
